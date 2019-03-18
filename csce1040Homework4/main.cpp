e2f893d80171d0741b8f326a1a47a9be691ccc0f
Andrew Sansom <andrewpsansom@gmail.com>
2019-03-18 16:10:34 -0500

    Initial Commit

---
+++ csce1040Homework4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  csce1040Homework4
+//
+//  Created by Andrew Sansom on 3/18/19.
+//  Copyright © 2019 Andrew Sansom. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
